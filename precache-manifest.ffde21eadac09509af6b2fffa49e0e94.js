018b6b0bc39a879d247d28469481e32b2ddcc915
Julian Orozco <orozco.castorena@outlook.com>
2020-04-28 18:18:32 -0500

    Updates

---
+++ precache-manifest.ffde21eadac09509af6b2fffa49e0e94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "881464a1d6f5687e7e83847369b767cc",
+    "url": "/react-udemy-1/index.html"
+  },
+  {
+    "revision": "b637ab275bfc4e180e5a",
+    "url": "/react-udemy-1/static/css/main.4e3ade0d.chunk.css"
+  },
+  {
+    "revision": "38595435f572e49d21a2",
+    "url": "/react-udemy-1/static/js/2.773654cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-udemy-1/static/js/2.773654cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b637ab275bfc4e180e5a",
+    "url": "/react-udemy-1/static/js/main.8a2b90eb.chunk.js"
+  },
+  {
+    "revision": "57a9b3cbc87a0094aa68",
+    "url": "/react-udemy-1/static/js/runtime-main.36ccf736.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/monsters-rolodex/precache-manifest.f7da3d8d9564a1da3a13f68031939987.js"
+  "/react-udemy-1/precache-manifest.ffde21eadac09509af6b2fffa49e0e94.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/monsters-rolodex/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-udemy-1/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
